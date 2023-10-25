7306817d8e47579f2ace7dd7ab9d9d9a7039ba40
정남용 <jny5360@gmail.com>
2023-10-25 17:43:39 +0900

    디렉토리 이름 수정 (남용)

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from metawave import views
+from config import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.MainPage) 
+    path("admin/", admin.site.urls),
+    path("", views.MainPage),
 ]
--- metawave/apps.py
+++ metawave/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HelloworldConfig(AppConfig):
+class MetaWaveConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "helloworld"
+    name = "metawave"
