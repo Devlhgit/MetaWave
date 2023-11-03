b119bc06e6f01b67f2f1b706ba2f28deb7682399
Namyong2 <140034014+Namyong2@users.noreply.github.com>
2023-11-03 15:36:28 +0900

    231103 1536
    MySQL DB와 Django ORM 연

--- config/views.py
+++ config/views.py
@@ -5,8 +5,7 @@ from metawave.models import picture
 
 def mainPage(request):
     if request.method == "POST":
-        # print(111, request.POST)
-        # print(222, request.FILES)
+
         name = request.POST['name']
         author = request.POST['author']
         pictures = request.FILES['picture']
@@ -14,16 +13,6 @@ def mainPage(request):
         picture_instance = picture(name=request.POST['name'], author=request.POST['author'], picture=request.FILES['picture'])
         picture_instance.save()
         
-
-        # uploaded_file = request.FILES.get('picture')
-
-        # if uploaded_file is not None:
-
-        #     print('uploaded_file:', uploaded_file)
-        #     context = {'uploaded_file' : uploaded_file.url}
-        #     print('context:', context)
-
-        # return render(request, 'recommend.html', context)
     
     return render(request, "mainPage.html")
 
