421984995a337a33f701dc9ecde57346dd5a834e
Namyong2 <140034014+Namyong2@users.noreply.github.com>
2023-11-21 16:01:19 +0900

    views.py, playSongML.py 병합완료

--- metawave/models.py
+++ metawave/models.py
@@ -137,10 +137,7 @@ class MoodthemePlaylist(models.Model):
 
 
 class inputPicture(models.Model):
-    # name = models.CharField(max_length=255, blank=True, null=True)
-    # author = models.CharField(max_length=255, blank=True, null=True)
     picture = models.ImageField('picture_url', upload_to='pictures/', null=True)
-    # musicGenre = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         managed = False
--- model/img_analysis.py
+++ model/img_analysis.py
@@ -5,7 +5,7 @@ import csv
 import os
 import pandas as pd
 
-csv_file_path = r"C:\Users\gjaischool\Desktop\MetaWave\MetaWave\model\input_image_analysis_results.csv"  # 결과 CSV 파일 경로 변경 필요
+csv_file_path = r"model\input_image_analysis_results.csv"  # 결과 CSV 파일 경로 변경 필요
 test = ""
 # 이미지 분석을 위한 함수를 정의합니다.
 def analyze_image(image_path):
