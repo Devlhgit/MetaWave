5d8a767c9d5a5af9c63be605a0bebefa4f9539a7
Namyong2 <140034014+Namyong2@users.noreply.github.com>
2023-11-21 10:17:11 +0900

    123

--- config/settings.py
+++ config/settings.py
@@ -110,12 +110,12 @@ USE_TZ = True
 # # Static files (CSS, JavaScript, Images)
 # # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = '/static/'
 
@@ -129,8 +129,6 @@ STATICFILES_DIRS = [
 # 이 디렉토리는 개발 중에는 사용하지 않습니다.
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # 이 경로를 배포용으로 변경합니다.
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
