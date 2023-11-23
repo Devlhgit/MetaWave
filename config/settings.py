acfb855278a2bd3890d3aa7af420a73b06ae0873
Devlhgit <Devlhgit>
2023-11-23 20:19:50 +0900

    11-23 08:19

--- config/settings.py
+++ config/settings.py
@@ -72,14 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 
-<<<<<<< HEAD
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
-}
-=======
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -90,7 +82,6 @@ DATABASES = {
 #         'PORT': '3306',
 #     }
 # }
->>>>>>> H
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
